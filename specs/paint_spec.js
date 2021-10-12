d69251a926d2caf3ba7a6f9e597a888d8240b497
Andrew Thomas <ajthomas145@gmail.com>
2021-10-12 20:31:07 +0100

    paint can tests complete

--- specs/paint_spec.js
+++ specs/paint_spec.js
@@ -19,6 +19,15 @@ describe("Paint", function(){
     });
 
     it("should be able to empty itself of paint", function(){
-        const actual = 
-    })
+        paint.emptyPaintCan()
+        const actual = paint.litres;
+        assert.strictEqual(actual, 0)
+    });
+
+    it("should be able to update that can is empty", function(){
+        paint.emptyPaintCan()
+        paint.updateCanEmpty()
+        const actual = paint.empty;
+        assert.strictEqual(actual, true)
+    });
 });
